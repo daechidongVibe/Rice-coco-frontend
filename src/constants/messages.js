f3ef02fa69d098e49b3e4e9668c17e5cd0d6a368
부트캠프 <mini_16@vanillacoding-Macmini-16.local>
2020-11-15 15:46:13 +0900

    Add SearchScreen

---
+++ src/constants/messages.js
@@ -0,0 +1,5 @@
+export const OBJECT ='object';
+export const OPEN ='open';
+export const CLOSE ='close';
+export const SHOULD_ENTER_WORD = '검색어를 입력해주세요';
+export const SHOULD_ENTER_FOOD = '먹고싶은 음식을 입력해주세요';
---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,90 @@
+import React, { useState } from 'react';
+import { FlatList, Keyboard } from 'react-native';
+import styled from 'styled-components/native';
+import { Input } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+
+import RenderItem from '../components/RenderItem';
+import configuredAxios from '../config/axiosConfig';
+import { SHOULD_ENTER_WORD, SHOULD_ENTER_FOOD } from '../constants/messages';
+import getEnvVars from '../../environment';
+const { REACT_NATIVE_GOOGLE_PLACES_URL, REACT_NATIVE_GOOGLE_PLACES_API_KEY } = getEnvVars();
+
+const Search = ({ navigation }) => {
+  const [searchList, setSearchList] = useState([]);
+  const [searchWord, setSearchWord] = useState('');
+  const [isSearching, setIsSearching] = useState(false);
+  const [placeHolder, setPlaceHolder] = useState(SHOULD_ENTER_FOOD);
+
+  const restaurantUrl = `${REACT_NATIVE_GOOGLE_PLACES_URL}&keyword=${searchWord}&key=${REACT_NATIVE_GOOGLE_PLACES_API_KEY}`;
+
+  const handleSearchIconClick = async () => {
+    if (isSearching) return;
+
+    if (!searchWord) {
+      setPlaceHolder(SHOULD_ENTER_WORD);
+      return;
+    }
+    Keyboard.dismiss();
+    setIsSearching(true);
+
+    try {
+      const { data: { results } } = await configuredAxios.get(restaurantUrl);
+      const filteredDataList = results.map(
+        result => {
+          return {
+            id: result.place_id,
+            name: result.name,
+            rating: result.rating,
+            openingHours: result['opening_hours'],
+          };
+        }
+      );
+
+      setSearchList(filteredDataList);
+      setSearchWord('');
+    } catch (err) {
+      console.log(err);
+    }
+
+    setIsSearching(false);
+  };
+
+  return (
+    <Container>
+      <Input
+        placeholder={placeHolder}
+        value={searchWord}
+        onChangeText={setSearchWord}
+        rightIcon={
+          <Icon
+            name='utensil-spoon'
+            size={30}
+            color='#ff914d'
+            onPress={handleSearchIconClick}
+          />
+        }
+      />
+      <FlatList
+        data={searchList}
+        renderItem={({ item }) => (
+          <RenderItem
+            item={item}
+            navigation={navigation}
+          />
+        )}
+        keyExtractor={item => item.id}
+      />
+    </Container>
+  );
+};
+
+const Container = styled.View`
+  flex: 1;
+  padding: 10px;
+  background-color: #ffffff;
+  text-align: center;
+  marginBottom: 20px;
+`;
+
+export default Search;
